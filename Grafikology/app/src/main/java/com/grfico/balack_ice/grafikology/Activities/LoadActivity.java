ef32efee724f6e80ace6560d56148f9dd4ec1ebe
Khaled Guesmi <khaled.guesmi@esprit.tn>
2019-02-13 00:12:46 +0100

    chonjmon test
    3malt chonjmon

---
+++ Grafikology/app/src/main/java/com/grfico/balack_ice/grafikology/Activities/LoadActivity.java
@@ -0,0 +1,16 @@
+package com.grfico.balack_ice.grafikology.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.grfico.balack_ice.grafikology.R;
+
+public class LoadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load);
+    }
+}
+//3asba kbira
---
+++ Grafikology/app/src/main/java/com/grfico/balack_ice/grafikology/offline/WelcomeOfflineActivity.java
@@ -0,0 +1,15 @@
+package com.grfico.balack_ice.grafikology.offline;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.grfico.balack_ice.grafikology.R;
+
+public class WelcomeOfflineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_offline);
+    }
+}
