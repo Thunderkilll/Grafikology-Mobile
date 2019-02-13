cebaa1e7e7e6f7eddf637f87b68e8b4a8ab7b819
WarefTounsi <47356647+WarefTounsi@users.noreply.github.com>
2019-02-13 13:54:31 +0100

    waref1.0
    Co-Authored-By: wareftounsi <wareftounsi@users.noreply.github.com>

--- Grafikology/app/src/main/java/com/grfico/balack_ice/grafikology/Activities/LoginActivity.java
+++ Grafikology/app/src/main/java/com/grfico/balack_ice/grafikology/Activities/LoginActivity.java
@@ -1,15 +1,31 @@
 package com.grfico.balack_ice.grafikology.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.grfico.balack_ice.grafikology.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextView registerButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        registerButton = findViewById(R.id.textView2);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
